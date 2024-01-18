0bf04b32605df639fdce92bf657b3588dbda68d4
Eigeen <dengyk2002@qq.com>
2024-01-18 16:55:26 +0800

    FFI: 添加Log接口

--- src/lua_core_ffi.h
+++ src/lua_core_ffi.h
@@ -29,4 +29,38 @@ extern "C" {
 			}
 		}
 	}
+
+	/// <summary>
+	/// <para>输出日志</para>
+	/// 
+	/// 日志等级：
+	/// <para>0 -> DEBUG</para>
+	/// <para>1 -> INFO</para>
+	/// <para>2 -> WARN</para>
+	/// <para>3 -> ERROR</para>
+	/// </summary>
+	/// <param name="level">
+	/// 日志等级
+	/// </param>
+	/// <param name="content">日志内容</param>
+	DllExport void Log(int level, const char* content) {
+		switch (level)
+		{
+		case 0:
+			engine_logger->debug(content);
+			break;
+		case 1:
+			engine_logger->info(content);
+			break;
+		case 2:
+			engine_logger->warn(content);
+			break;
+		case 3:
+			engine_logger->error(content);
+			break;
+		default:
+			engine_logger->error(content);
+			break;
+		}
+	}
 }
