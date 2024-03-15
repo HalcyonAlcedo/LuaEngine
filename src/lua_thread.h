7531898964bc55c99615ed8be9b263e7bc259eeb
zyc404 <zyc404@gmail.com>
2024-03-15 17:10:43 +0800

    添加协程中错误捕获

--- src/lua_thread.h
+++ src/lua_thread.h
@@ -12,8 +12,18 @@ namespace lua_thread {
 			sol::thread t = sol::thread::create(lua);
 			// 从 sol::thread 中获取 sol::state_view
 			sol::state_view new_lua = t.state();
+			// 将任务函数包装在 protected_function 中
+			sol::protected_function protected_task_func(task_func);
 			// 在新的 sol::state_view 中执行任务函数
-			sol::object result = task_func.call(new_lua);
+			sol::protected_function_result result = protected_task_func(new_lua);
+			// 检查执行是否成功
+			if (!result.valid()) {
+				// 获取错误信息
+				sol::error err = result;
+				std::string what = err.what();
+				lua_logger->error("LuaEngine Thread Error:\n" + what);
+				LOG(ERR) << "LuaEngine Thread Error:\n" + what;
+			}
 			});
 		t.detach();
 	}
